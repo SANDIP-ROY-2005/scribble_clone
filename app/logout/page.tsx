1e60c9e0bce773b4df0e6cba145a9999f22a2251
SANDIP-ROY-2005 <161707774+SANDIP-ROY-2005@users.noreply.github.com>
2025-07-15 18:14:43 +0530

    shadcn setup

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       page by sandip
+      <Button className='bg-red-500'>Logout</Button>
     </div>
   )
 }
